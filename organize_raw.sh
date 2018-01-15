d505ca6b9b8ff9df53aa07df0e72e527eeb08a44
Kai Hwang <kai.hwang@gmail.com>
2018-01-15 11:18:26 -0800

    fMRI pilot.

---
+++ organize_raw.sh
@@ -0,0 +1,10 @@
+
+
+WD='/home/despoB/kaihwang/HTB_fMRI'
+
+for s in Pilot01; do
+	
+	heudiconv -d ${WD}/Raw/{subject}/*/*/* -s ${s} \
+	-f /home/despoB/kaihwang/bin/HTB-MNE/TTD_heuristics.py -c dcm2niix -o ${WD}/BIDS --bids
+
+done
---
+++ run_fmriprep.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source /home/despoB/kaihwang/.bashrc;
+source activate fmriprep;
+#SUB_ID="${SGE_TASK}";
+WD='/home/despoB/kaihwang/HTB_fMRI/'
+#SCRIPTS='/home/despoB/kaihwang/bin/TTD/Preprocessing'
+
+#cd ${WD}/fmriprep;
+
+fmriprep \
+    --participant_label Pilot01 \
+    --nthreads 4 \
+    --output-space T1w template \
+    --template MNI152NLin2009cAsym \
+    ${WD}/BIDS/ \
+    ${WD}/fmriprep/ \
+    participant
+
+#END_TIME=$(date);
+#echo "fMRIprep for $SUB_ID completed at $END_TIME";
