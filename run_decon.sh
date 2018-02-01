a5d0b469920aec119928cdcc08a9f27b58e6381f
Kai Hwang <kai.hwang@gmail.com>
2018-02-01 15:55:21 -0800

    fix FIR duration error.

--- run_decon.sh
+++ run_decon.sh
@@ -57,21 +57,23 @@ for s in Pilot03; do
 	-num_stimts 4 \
 	-censor ${Output}/sub-${s}/FD0.2_censor.1D \
 	-ortvec ${Output}/sub-${s}/confounds.tsv confounds \
-	-stim_times 1 /home/despoB/kaihwang/bin/HTB-MNE/${s}_R4 'TENT(0, 7, 7)' -stim_label 1 R4 \
-	-stim_times 2 /home/despoB/kaihwang/bin/HTB-MNE/${s}_R8 'TENT(0, 7, 7)' -stim_label 2 R8 \
-	-stim_times 3 /home/despoB/kaihwang/bin/HTB-MNE/${s}_D1 'TENT(0, 7, 7)' -stim_label 3 D1 \
-	-stim_times 4 /home/despoB/kaihwang/bin/HTB-MNE/${s}_D2 'TENT(0, 7, 7)' -stim_label 4 D2 \
+	-stim_times 1 /home/despoB/kaihwang/bin/HTB-MNE/${s}_R4 'TENT(0, 14, 8)' -stim_label 1 R4 \
+	-stim_times 2 /home/despoB/kaihwang/bin/HTB-MNE/${s}_R8 'TENT(0, 14, 8)' -stim_label 2 R8 \
+	-stim_times 3 /home/despoB/kaihwang/bin/HTB-MNE/${s}_D1 'TENT(0, 14, 8)' -stim_label 3 D1 \
+	-stim_times 4 /home/despoB/kaihwang/bin/HTB-MNE/${s}_D2 'TENT(0, 14, 8)' -stim_label 4 D2 \
 	-iresp 1 ${Output}/sub-${s}/R4_FIR.nii.gz \
 	-iresp 2 ${Output}/sub-${s}/R8_FIR.nii.gz \
 	-iresp 3 ${Output}/sub-${s}/D1_FIR.nii.gz \
 	-iresp 4 ${Output}/sub-${s}/D2_FIR.nii.gz \
-	-gltsym 'SYM: +1*D1 +1*D2 -1*R4 -1*R8 ' -glt_label 1 D1+D2-R4+R8 \
-	-gltsym 'SYM: +1*D2 +1*R8 -1*D1 -1*R4 ' -glt_label 2 D2+R8-D1-R4 \
-	-gltsym 'SYM: +1*R8 -1*R4 ' -glt_label 3 R8-R4 \
-	-gltsym 'SYM: +1*D2 -1*D1 ' -glt_label 4 D2-D1 \
-	-gltsym 'SYM: +1*D2 -1*R8 ' -glt_label 5 R8-D2 \
-	-gltsym 'SYM: +1*D1 -1*R4 ' -glt_label 6 D1-R4 \
-	-gltsym 'SYM: +1*D2 -1*R4 ' -glt_label 7 D2-R4 \
+	-gltsym 'SYM: +1*D1[1..4] +1*D2[1..4] -1*R4[1..4] -1*R8[1..4] ' -glt_label 1 D1+D2-R4+R8 \
+	-gltsym 'SYM: +1*D2[1..4] +1*R8[1..4] -1*D1[1..4] -1*R4[1..4] ' -glt_label 2 D2+R8-D1-R4 \
+	-gltsym 'SYM: +1*R8[1..4] -1*R4[1..4] ' -glt_label 3 R8-R4 \
+	-gltsym 'SYM: +1*D2[1..4] -1*D1[1..4] ' -glt_label 4 D2-D1 \
+	-gltsym 'SYM: +1*D2[1..4] -1*R8[1..4] ' -glt_label 5 R8-D2 \
+	-gltsym 'SYM: +1*D1[1..4] -1*R4[1..4] ' -glt_label 6 D1-R4 \
+	-gltsym 'SYM: +1*D2[1..4] -1*R4[1..4] ' -glt_label 7 D2-R4 \
+	-gltsym 'SYM: +0.5*R4[1..4] +0.5*R8[1..4] ' -glt_label 8 R4+R8 \
+	-gltsym 'SYM: +0.5*D1[1..4] +0.5*D2[1..4] ' -glt_label 9 D1+D2 \
 	-rout \
 	-tout \
 	-bucket ${Output}/sub-${s}/FIRmodel_task-HTBSBTR2_stats.nii.gz \
